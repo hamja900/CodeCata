5e6077988a3e4452fb427c0d87ba5277840c4aaf
hamja900 <luru@naver.com>
2024-01-11 09:08:12 +0900

    001.두 수의 차

---
+++ 01.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeCata
+{
+    internal class Program
+    {
+        int solution(int num1, int num2)
+        {
+            int answer = 0;
+            if (-50000 <= num1 && num1 <= 50000 && -50000 <= num2 && num2 <= 50000)
+            {
+                answer = num1 - num2;
+            }
+            return answer;
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+}
